9b5dd3db8e14771deaee1a29007068ff9e4c05b2
Balch <jay@blackpixel.com>
2017-05-28 11:08:09 -0700

    update copyright

--- app/src/main/java/com/balch/auctionbrowser/AuctionApplication.kt
+++ app/src/main/java/com/balch/auctionbrowser/AuctionApplication.kt
@@ -1,23 +1,22 @@
 /*
- *  Author: Balch
- *  Created: 6/14/15 11:21 AM
+ * Author: Balch
  *
- *  This file is part of BB_Challenge.
+ * This file is part of AuctionBrowser.
  *
- *  BB_Challenge is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * AuctionBrowser is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  BB_Challenge is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * AuctionBrowser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with MockTrade.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with MockTrade.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  Copyright (C) 2015
+ * Copyright (C) 2017
  *
  */
 
--- app/src/main/java/com/balch/auctionbrowser/auction/AuctionView.kt
+++ app/src/main/java/com/balch/auctionbrowser/auction/AuctionView.kt
@@ -1,15 +1,14 @@
 /*
  * Author: Balch
- * Created: 9/4/14 12:26 AM
  *
- * This file is part of MockTrade.
+ * This file is part of AuctionBrowser.
  *
- * MockTrade is free software: you can redistribute it and/or modify
+ * AuctionBrowser is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MockTrade is distributed in the hope that it will be useful,
+ * AuctionBrowser is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -17,7 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with MockTrade.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2014
+ * Copyright (C) 2017
+ *
  */
 
 package com.balch.auctionbrowser.auction
