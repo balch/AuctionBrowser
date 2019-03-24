f38c5e48b52dc6cc4e04ca329d11f163374e0213
balch <balch61@gmail.com>
2019-03-24 05:36:52 -0700

    first espresso test!!!

---
+++ app/src/androidTest/java/com/balch/auctionbrowser/MainActivityTest.kt
@@ -0,0 +1,89 @@
+/*
+ * Author: Balch
+ *
+ * This file is part of AuctionBrowser.
+ *
+ * AuctionBrowser is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AuctionBrowser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MockTrade.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2019
+ *
+ */
+
+/*
+ * Author: Balch
+ *
+ * This file is part of AuctionBrowser.
+ *
+ * AuctionBrowser is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AuctionBrowser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MockTrade.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2019
+ *
+ */
+
+package com.balch.auctionbrowser
+
+import android.widget.EditText
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.squareup.rx2.idler.Rx2Idler
+import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Before
+    fun setup() {
+        RxJavaPlugins.setInitIoSchedulerHandler(
+                Rx2Idler.create("RxJava 2.x Computation Scheduler"));    }
+
+    @Test
+    fun testAuctionSearch() {
+
+        // preform Auction Search for RC
+        onView(withId(R.id.menu_search)).perform(click())
+
+        onView(isAssignableFrom(EditText::class.java))
+                .perform(typeText("RC"),
+                        pressImeActionButton())
+
+
+        // Check that the text was changed.
+        onView(isAssignableFrom(EditText::class.java))
+            .check(matches(withText("RC")))
+
+    }
+
+}
